311b44535d763d73e05be8248fa510457a5a3c10
Atul Patle <atulpatle006@gmail.com>
2026-09-23 19:34:12 +0530

    Add automated tesing for login

---
+++ tests/test_login.py
@@ -0,0 +1,31 @@
+from fastapi.testclient import TestClient
+from main import app
+from unittest.mock import MagicMock, patch
+
+client = TestClient(app)
+
+def test_login_success():
+    fake_cursor = MagicMock()
+
+    fake_cursor.fetchone.return_value = {
+        "email": "atul@gmail.com",
+        "password": "fake_hashed_pass"
+    }
+
+    fake_connection = MagicMock()
+    fake_connection.cursor.return_value = fake_cursor
+
+    with patch("main.get_connection", return_value = fake_connection):
+        with patch("main.pwd_context.verify", return_value = True):
+            response = client.post(
+                "/login",
+                json={
+                    "email": "atul@gmail.com",
+                    "password": "atul@123"
+                }
+            )
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "Login Successfull!"
+    }
