c14f72e68128ffead689e58374b529ee1f68e9ee
Atul Patle <atulpatle006@gmail.com>
2026-09-23 23:02:26 +0530

    Add automated testing in register API

---
+++ tests/test_register.py
@@ -0,0 +1,31 @@
+from fastapi.testclient import TestClient
+from main import app
+
+from unittest.mock import MagicMock, patch
+
+client = TestClient(app)
+
+def test_register_success():
+    fake_cursor = MagicMock()
+
+    fake_cursor.fetchone.return_value = None 
+
+    fake_connection = MagicMock()
+    fake_connection.cursor.return_value = fake_cursor
+
+    with patch("main.get_connection", return_value= fake_connection):
+        with patch("main.pwd_context.hash", return_value = "fake_hashed_pass"):
+            response = client.post(
+                "/register",
+                json = {
+                    "email": "atul@gmail.com",
+                    "year": 2005,
+                    "password": "atul@123",
+                    "conf_password": "atul@123"
+                }
+            )
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "Registration Successfull!"
+    }
