1d0fabe19499a6a764fa0857a505ac811199951f
Atul Patle <atulpatle006@gmail.com>
2026-09-24 22:50:52 +0530

    Add more test cases in register endpoint

--- tests/test_register.py
+++ tests/test_register.py
@@ -29,3 +29,43 @@ def test_register_success():
     assert response.json() == {
         "message": "Registration Successfull!"
     }
+
+def test_register_wrong_password():
+    
+    response = client.post(
+            "/register",
+            json = {
+                "email": "atul@gmail.com",
+                "year": 2005,
+                "password": "atul@123",
+                "conf_password": "atul@444"
+            }
+        )
+    
+    assert response.status_code == 422
+
+def test_email_already_exixts():
+    fake_cursor = MagicMock()
+
+    fake_cursor.fetchone.return_value = {
+        "email": "atul@gmail.com",
+        "year": 2005,
+        "password": "fake_hash_pass"
+    }
+
+    fake_connection = MagicMock()
+
+    fake_connection.cursor.return_value = fake_cursor
+
+    with patch("main.get_connection", return_value = fake_connection):
+        response = client.post(
+            "/register",
+            json={
+                "email": "atul@gmail.com",
+                "year": 2005,
+                "password": "atul@123",
+                "conf_password": "atul@123"
+            }
+        )
+
+    assert response.status_code == 500
