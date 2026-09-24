10c3937b01304c5d13c7114426bdcf8099caa599
Atul Patle <atulpatle006@gmail.com>
2026-09-24 23:15:58 +0530

    Add fixture to reduce repeating lines of code

--- tests/test_register.py
+++ tests/test_register.py
@@ -1,23 +1,32 @@
+import pytest
 from fastapi.testclient import TestClient
 from main import app
-
 from unittest.mock import MagicMock, patch
 
 client = TestClient(app)
 
-def test_register_success():
+
+@pytest.fixture
+def fake_db():
     fake_cursor = MagicMock()
-
-    fake_cursor.fetchone.return_value = None 
-
     fake_connection = MagicMock()
+
     fake_connection.cursor.return_value = fake_cursor
 
-    with patch("main.get_connection", return_value= fake_connection):
-        with patch("main.pwd_context.hash", return_value = "fake_hashed_pass"):
+    return fake_connection, fake_cursor
+
+
+def test_register_success(fake_db):
+    fake_connection, fake_cursor = fake_db
+
+    fake_cursor.fetchone.return_value = None
+
+    with patch("main.get_connection", return_value=fake_connection):
+        with patch("main.pwd_context.hash", return_value="fake_hashed_pass"):
+
             response = client.post(
                 "/register",
-                json = {
+                json={
                     "email": "atul@gmail.com",
                     "year": 2005,
                     "password": "atul@123",
@@ -26,26 +35,29 @@ def test_register_success():
             )
 
     assert response.status_code == 200
+
     assert response.json() == {
         "message": "Registration Successfull!"
     }
 
+
 def test_register_wrong_password():
-    
+
     response = client.post(
-            "/register",
-            json = {
-                "email": "atul@gmail.com",
-                "year": 2005,
-                "password": "atul@123",
-                "conf_password": "atul@444"
-            }
-        )
-    
+        "/register",
+        json={
+            "email": "atul@gmail.com",
+            "year": 2005,
+            "password": "atul@123",
+            "conf_password": "atul@444"
+        }
+    )
+
     assert response.status_code == 422
 
-def test_email_already_exixts():
-    fake_cursor = MagicMock()
+
+def test_email_already_exists(fake_db):
+    fake_connection, fake_cursor = fake_db
 
     fake_cursor.fetchone.return_value = {
         "email": "atul@gmail.com",
@@ -53,11 +65,8 @@ def test_email_already_exixts():
         "password": "fake_hash_pass"
     }
 
-    fake_connection = MagicMock()
+    with patch("main.get_connection", return_value=fake_connection):
 
-    fake_connection.cursor.return_value = fake_cursor
-
-    with patch("main.get_connection", return_value = fake_connection):
         response = client.post(
             "/register",
             json={
